c034f0354089c7e49aa0e62adde2cfb1bdd7d9b2
Ftfs <49188843+4257@users.noreply.github.com>
2023-08-01 18:39:29 +0800

    Update filePEread.c

--- Code/filePEread.c
+++ Code/filePEread.c
@@ -3,220 +3,167 @@
 #include <windows.h>
 #include <winnt.h>
 
-
-DWORD loadFile(const LPSTR file_path,LPVOID* file_buff){
+//函数声明
+//**************************************************************************
+//ReadPEFile:将文件读取到缓冲区
+//参数说明：
+//lpszFile 文件路径
+//pFileBuffer 缓冲区指针
+//返回值说明：
+//读取失败返回0  否则返回实际读取的大小
+//**************************************************************************
+DWORD ReadPEFile(IN LPSTR lpszFile,OUT LPVOID* pFileBuffer){
     //文件指针
     FILE* pfile = NULL;
-    //文件大小
-    DWORD file_size = 0;
-    //接受DWORD类型的返回值，判断
-    DWORD flag = 0;
-
-    if(!(pfile = fopen(file_path,"rb"))){
-        printf("Can`t open the file!\n");
-        return 0;
-    };
-    //将文件指针指向最后
-    fseek(pfile,0,SEEK_END);
-    //ftell函数获取文件指针当前的位置。相当于文件大小
-    file_size = ftell(pfile);
-    //将文件指针指向开头
-    fseek(pfile,0,SEEK_SET);
-    //判断传入的参数file_buff是否申请到内存
-    if(!(*file_buff = malloc(file_size))){
-		printf("Can`t allocation buff!\n");
-		fclose(pfile);
-		return 0;
-    };
     
-    flag = fread(*file_buff,file_size,sizeof(char),pfile);
-    if (!flag)
-    {
+    //打开文件，并将返回的FILE* 给创建的文件指针
+    //判断pfile是否不为NULL
+    if(!(pfile = fopen(lpszFile,"rb"))){
+        printf("Can`t open the file!\n");
+        exit(1);
+    }
+
+    //传入一个FILE* 指针 设置偏移为0 且指向文件末尾
+    fseek(pfile,0,SEEK_END);
+
+    //文件大小
+    DWORD fileSize;
+    //返回给定流的当前文件位置
+    fileSize = ftell(pfile);
+    // printf("This file is %d bytes!\n",fileSize); 
+
+    //将文件指针设置到文件开头
+    fseek(pfile,0,SEEK_SET);
+
+    if(!(*pFileBuffer = malloc(fileSize))){
+        printf("Can`t malloc filebuffer!\n");
+        fclose(pfile);
+    }
+    printf("malloc %d bytes\n",fileSize);
+
+    //写入大小
+    DWORD wriSize;
+    wriSize = fread(*pFileBuffer,1,fileSize,pfile);
+    if (!wriSize){
         printf("Can`t copy file to buff!\n");
         fclose(pfile);
-        free(*file_buff);
         return 0;
     }
+
     //关闭文件
     fclose(pfile);
-    return file_size;
+    //返回写入Buff的大小
+    return wriSize;
+
 }
 
-DWORD CopyFileBufferToImageBuffer(LPVOID file_buff,LPVOID* image_buff){
+//**************************************************************************
+//CopyFileBufferToImageBuffer:将文件从FileBuffer复制到ImageBuffer
+//参数说明：
+//pFileBuffer  FileBuffer指针
+//pImageBuffer ImageBuffer指针
+//返回值说明：
+//读取失败返回0  否则返回复制的大小
+//**************************************************************************
+DWORD CopyFileBufferToImageBuffer(IN LPVOID pFileBuffer,OUT LPVOID* pImageBuffer){
     PIMAGE_DOS_HEADER pDh = NULL;
-    PIMAGE_NT_HEADERS32 pNh = NULL;
+    PIMAGE_NT_HEADERS pN32h = NULL;
+    PIMAGE_NT_HEADERS64 pN64h = NULL;
     PIMAGE_FILE_HEADER pFh = NULL;
     PIMAGE_OPTIONAL_HEADER pO32h = NULL;
     PIMAGE_OPTIONAL_HEADER64 pO64h = NULL;
     PIMAGE_SECTION_HEADER pSh = NULL;
-    // LPVOID image_buff = NULL;
-    BYTE text[9] = {0};
 
-    pDh = (PIMAGE_DOS_HEADER)file_buff;
-    //判断是否位MZ标志位
-    // printf("%p",pDh->e_magic);
-    if (pDh->e_magic != IMAGE_DOS_SIGNATURE)
-    {
-		printf("Not a valid PE file! Error by Dos header!\n");
-        free(file_buff);
-		return 0;
+    //将FileBuffer指向DOS头 
+    pDh = (PIMAGE_DOS_HEADER)pFileBuffer;
+    if(pDh->e_magic != IMAGE_DOS_SIGNATURE){
+        printf("Not a valid PE file! Error by Dos header!\n");
+        exit(1);
     }
 
-    pNh = (PIMAGE_NT_HEADERS32)((DWORD)pDh + pDh->e_lfanew);
-    //判断是否为PE标志位
-    if (pNh->Signature != IMAGE_NT_SIGNATURE)
-    {
-		printf("Not a valid PE file! Error by NT header!\n");
-        free(file_buff);
-		return 0;
+    //DOS头加上DOS头偏移找到NT头
+    pN32h = (PIMAGE_NT_HEADERS)((DWORD)pDh + pDh->e_lfanew);
+    //判断软件的IMAGE_NT_SIGNATURE
+    if (pN32h->Signature != IMAGE_NT_SIGNATURE){
+        printf("Not a valid PE file! Error by NT header!\n");
+        exit(1);
     }
 
-    pFh = (PIMAGE_FILE_HEADER)&(pNh->FileHeader);
-    //判断是x86pe文件还是x64pe文件
-    if (pFh->Machine == IMAGE_FILE_MACHINE_AMD64)
-    {
-        pO64h = (PIMAGE_OPTIONAL_HEADER64)&(pNh->OptionalHeader);
-        if(!(*image_buff = malloc(pO64h->SizeOfImage))){
-            printf("Can`t allocation buff!\n");
-            free(*image_buff);
-            return 0;
-        }
-        memset(*image_buff,0,sizeof(int));
-        (*image_buff) = memcpy(*image_buff,file_buff,pO64h->SizeOfHeaders);
-        for (size_t i = 1,j = 0; i <= pFh->NumberOfSections; i++)
-        {
-            pSh = (PIMAGE_SECTION_HEADER)((DWORD)pFh+sizeof(IMAGE_FILE_HEADER)+pFh->SizeOfOptionalHeader+j);
-            memcpy((LPVOID)((DWORD)(*image_buff)+pSh->VirtualAddress),(LPVOID)((DWORD)file_buff+pSh->PointerToRawData),pSh->SizeOfRawData);
-            j += 0x28;
-        }
-        // FILE* filetest;
-        // filetest = fopen("D:\\justdo\\A\\imagebuff.txt","wb");
-        // fwrite(image_buff,sizeof(char),pO64h->SizeOfImage,filetest);
-        // fclose(filetest);
-        return pO64h->SizeOfImage;
-    }else if (pFh->Machine == IMAGE_FILE_MACHINE_I386)
-    {
-        pO32h = (PIMAGE_OPTIONAL_HEADER32)&(pNh->OptionalHeader);
-        // printf("%p\n",pO32h->SizeOfImage);
-        if(!(*image_buff = malloc(pO32h->SizeOfImage))){
-            printf("Can`t allocation buff!\n");
-            free(*image_buff);
-            return 0;
-        }
-        memset(*image_buff,0,sizeof(int));
-        *image_buff = memcpy(*image_buff,file_buff,pO32h->SizeOfHeaders);
-        for (size_t i = 1,j = 0; i <= pFh->NumberOfSections; i++)
-        {
-            pSh = (PIMAGE_SECTION_HEADER)((DWORD)pFh+sizeof(IMAGE_FILE_HEADER)+pFh->SizeOfOptionalHeader+j);
-            memcpy((LPVOID)((DWORD)(*image_buff)+pSh->VirtualAddress),(LPVOID)((DWORD)file_buff+pSh->PointerToRawData),pSh->SizeOfRawData);
-            j += 0x28;
-        }
-        return pO32h->SizeOfImage;
-    }
-}
-
-DWORD CopyImageBufferToNewBuffer(LPVOID pImageBuffer,LPVOID* pNewBuffer){
-    PIMAGE_DOS_HEADER pDh = NULL;
-    PIMAGE_NT_HEADERS32 pNh = NULL;
-    PIMAGE_FILE_HEADER pFh = NULL;
-    PIMAGE_OPTIONAL_HEADER pO32h = NULL;
-    PIMAGE_OPTIONAL_HEADER64 pO64h = NULL;
-    //最后一个节的位置
-    PIMAGE_SECTION_HEADER lastSection = NULL;
-    PIMAGE_SECTION_HEADER pSh = NULL;
-    //节数
-    DWORD numOFsections = 0;
-    //IMAGE_BUFF计算的文件大小
-    DWORD file_Size = 0;
-
-    pDh = (PIMAGE_DOS_HEADER)pImageBuffer;
-    pNh = (PIMAGE_NT_HEADERS32)((DWORD)pDh+pDh->e_lfanew);
-    pFh = (PIMAGE_FILE_HEADER)&pNh->FileHeader;
-    numOFsections = pFh->NumberOfSections - 1;
-    lastSection = (PIMAGE_SECTION_HEADER)((DWORD)pFh+sizeof(IMAGE_FILE_HEADER)+pFh->SizeOfOptionalHeader+(numOFsections*sizeof(IMAGE_SECTION_HEADER)));
-    if (numOFsections <= 1)
-    {
-        printf("File error!\n");
-        exit(0);
-    }
-    // printf("%p\n",lastSection->PointerToRawData);
-    // printf("%p\n",lastSection->SizeOfRawData);
-    file_Size = (lastSection->PointerToRawData)+(lastSection->SizeOfRawData);
-    // printf("%p\n",file_Size);
+    pFh = (PIMAGE_FILE_HEADER)&(pN32h->FileHeader);
+    //判断软件是64位还是32位
     if (pFh->Machine == IMAGE_FILE_MACHINE_AMD64){
-        pO64h = (PIMAGE_OPTIONAL_HEADER64)&pNh->OptionalHeader;
-        *pNewBuffer = malloc(file_Size);
-        if (!(*pNewBuffer))
-        {
-            printf("Can`t allocation buff!\n");
-            free(*pNewBuffer);
-            return 0;   
-        }
-        memset(*pNewBuffer,0,sizeof(int));
-        (*pNewBuffer) = memcpy(*pNewBuffer,pImageBuffer,pO64h->SizeOfHeaders);
-        for (size_t i = 1,j = 0; i <= pFh->NumberOfSections; i++)
-        {
-            pSh = (PIMAGE_SECTION_HEADER)((DWORD)pFh+sizeof(IMAGE_FILE_HEADER)+pFh->SizeOfOptionalHeader+j);
-            memcpy((LPVOID)((DWORD)(*pNewBuffer)+pSh->PointerToRawData),(LPVOID)((DWORD)pImageBuffer+pSh->VirtualAddress),pSh->SizeOfRawData);
-            j += 0x28;
-        }
-        return file_Size;
-    }else if (pFh->Machine == IMAGE_FILE_MACHINE_I386)
-    {
-        pO32h = (PIMAGE_OPTIONAL_HEADER32)&pNh->OptionalHeader;
-        *pNewBuffer = malloc(file_Size);
-        if (!(*pNewBuffer))
-        {
-            printf("Can`t allocation buff!\n");
-            free(*pNewBuffer);
-            return 0;   
-        }
-        memset(*pNewBuffer,0,sizeof(int));
-        (*pNewBuffer) = memcpy(*pNewBuffer,pImageBuffer,pO32h->SizeOfHeaders);
-        for (size_t i = 1,j = 0; i <= pFh->NumberOfSections; i++)
-        {
-            pSh = (PIMAGE_SECTION_HEADER)((DWORD)pFh+sizeof(IMAGE_FILE_HEADER)+pFh->SizeOfOptionalHeader+j);
-            memcpy((LPVOID)((DWORD)(*pNewBuffer)+pSh->PointerToRawData),(LPVOID)((DWORD)pImageBuffer+pSh->VirtualAddress),pSh->SizeOfRawData);
-            j += 0x28;
-        }
-        return file_Size;
-    }  
+        pN64h = (PIMAGE_NT_HEADERS)((DWORD)pDh + pDh->e_lfanew);
+        pFh = (PIMAGE_FILE_HEADER)&(pN64h->FileHeader);
+        pO64h = (PIMAGE_OPTIONAL_HEADER64)&(pN64h->OptionalHeader);
+        DWORD Size_Image;
+        Size_Image = pO64h->SizeOfImage;
+        printf("SizeOfImage: %d",Size_Image);
+        DWORD Size_Header;
+        Size_Header = pO64h ->SizeOfHeaders;
+        printf("SizeOfImage: %d",Size_Header);
+    }else if(pFh->Machine == IMAGE_FILE_MACHINE_I386){
+        pN32h = (PIMAGE_NT_HEADERS)((DWORD)pDh + pDh->e_lfanew);
+        pFh = (PIMAGE_FILE_HEADER)&(pN32h->FileHeader);
+        pO32h = (PIMAGE_OPTIONAL_HEADER64)&(pN32h->OptionalHeader);
+    }
+    
+    
+    
+
 }
 
-DWORD saveFile(LPSTR newFile_path,LPVOID* filebuff,DWORD fileSize){
-    FILE* filetest;
-    filetest = fopen(newFile_path,"wb");
-    fwrite(*filebuff,sizeof(char),fileSize,filetest);
-    fclose(filetest);
-    return 0;
-}
 
+//**************************************************************************
+//CopyImageBufferToNewBuffer:将ImageBuffer中的数据复制到新的缓冲区
+//参数说明：
+//pImageBuffer ImageBuffer指针
+//pNewBuffer NewBuffer指针
+//返回值说明：
+//读取失败返回0  否则返回复制的大小
+//**************************************************************************
+DWORD CopyImageBufferToNewBuffer(IN LPVOID pImageBuffer,OUT LPVOID* pNewBuffer);
+
+
+//**************************************************************************
+//MemeryTOFile:将内存中的数据复制到文件
+//参数说明：
+//pMemBuffer 内存中数据的指针
+//size 要复制的大小
+//lpszFile 要存储的文件路径
+//返回值说明：
+//读取失败返回0  否则返回复制的大小
+//**************************************************************************
+BOOL MemeryTOFile(IN LPVOID pMemBuffer,IN size_t size,OUT LPSTR lpszFile);
+
+
+//**************************************************************************
+//RvaToFileOffset:将内存偏移转换为文件偏移
+//参数说明：
+//pFileBuffer FileBuffer指针
+//dwRva RVA的值
+//返回值说明：
+//返回转换后的FOA的值  如果失败返回0
+//**************************************************************************
+DWORD RvaToFileOffset(IN LPVOID pFileBuffer,IN DWORD dwRva);
 
 int main(int argc, char const *argv[])
 {
-    const LPSTR file_path = "D:\\justdo\\A\\PETool.exe";
-    LPSTR newFile_path = "D:\\justdo\\A\\newFilePETool.exe";
-    LPVOID file_buff = NULL;
-    LPVOID image_buff = NULL;
-    LPVOID file_newBuff = NULL;
+    LPVOID OrginFile = NULL;
+    LPVOID FileImage = NULL;
+    LPVOID NewFile = NULL;   
 
-    DWORD fileSize = 0;
-    DWORD NewfileSize = 0;
-    DWORD flag = 0;
+    //文件路径
+    LPSTR FilePath ="D:/justdo/A/PETool.exe";
 
-    fileSize = loadFile(file_path,&file_buff);
-    printf("FILE_BUFF-Size:%p\n",fileSize);
-    flag = CopyFileBufferToImageBuffer(file_buff,&image_buff);
-    printf("IMAGE_BUFF-Size:%p\n",flag);
-    NewfileSize = CopyImageBufferToNewBuffer(image_buff,&file_newBuff);
-    printf("NEWFILE_BUFF-Size:%p\n",NewfileSize);
+    //返回的文件大小
+    DWORD FileSize;
 
-    saveFile(newFile_path,&file_newBuff,fileSize);
+    //传入文件路径和void**类型的待申请地址空间
+    FileSize= ReadPEFile(FilePath,&OrginFile);
+    printf("writed file %d betys\n",FileSize);
 
+    CopyFileBufferToImageBuffer(OrginFile,&FileImage);
 
-
-    free(file_newBuff);
-    free(file_buff);
-    free(image_buff);
+    free(OrginFile);
     return 0;
 }
