85a028f35d7c06bad68ddbd4fdd5ccd19549c27c
Ftfs <49188843+4257@users.noreply.github.com>
2023-12-14 17:05:42 +0800

    Create signal.c

---
+++ Code/tcpip_network/10signal/signal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+//回调函数 信号处理函数 信号处理器
+void signal_handler_fun(int signum) {
+    printf("catch signal %d\n", signum);
+}
+
+void timeout(int sig){
+    if(sig == SIGALRM){
+        printf("Timeout!\n");
+        alarm(5);
+    }
+}
+
+//回调函数 信号处理函数 信号处理器
+void key_control(int sig){
+    if(sig == SIGINT){
+        fputs("CTRL+C pressed\n",stdout);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    //注册信号
+    signal(SIGINT, signal_handler_fun);
+    signal(SIGALRM,timeout);
+    alarm(5);
+    //程序运行
+    for (int i = 0; i < 3; ++i) {
+        printf("wait...\n");
+        sleep(100);
+    }
+    return 0;
+}
